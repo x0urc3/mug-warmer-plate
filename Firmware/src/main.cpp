23f968d733ff0c1b3b50ec9d8ef9ba1820a28f00
x0urc3 <xource@gmail.com>
2022-05-20 08:44:33 +0800

    Added design notes and capitalized pins definition

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,23 +1,37 @@
+/* (c) 2022 Khairulmizam Samsudin <xource@gmail.com
+ *
+ * Firmware for mug warmer plate  
+ */
+
+/* Design Notes
+ * - Switch pin should use AVR internal pullup
+ * - Temperature sensor Vout connected to PB5/Reset
+ * 	- ATTiny85 PB5/RESET would reset if input <0.3*Vcc i.e. 0.3*5=1.5V
+ * 	- LM35 sensor 200-800mV for temperature between 20-80 celcius i.e. 10mV/Celcius 
+ * 	- LM35 Vout connected to ground with 18KOhm can measure value between -55 to 150 celcius
+ * 		- Resolution would be reduced
+ */
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 //#include <TinySoftwareSerial.h> //Use Pin 5/0 and Pin 6/1 on ATTiny85/Arduino
 //SoftwareSerial mySerial(4, 3); // RX, TX
 
 // pinout                Phy  |   AVR |   Arduino
-#define ledPin1     1  // 6       PB1         1
-#define ledPin2     3  // 2       PB3         3
-#define ledPin3     4  // 3       PB4         4
-#define gatePin     0  // 5       PB0         0
-#define tempPin     A0 // 1       PB5         A0
-#define switchPin   2  // 7       PB2         2
+#define LEDPIN1     1  // 6       PB1         1
+#define LEDPIN2     3  // 2       PB3         3
+#define LEDPIN3     4  // 3       PB4         4
+#define GATEPIN     0  // 5       PB0         0
+#define TEMPPIN     A0 // 1       PB5         A0
+#define SWITCHPIN   2  // 7       PB2         2
+
 
 void setup() {
-    pinMode(ledPin1, OUTPUT);
-    pinMode(ledPin2, OUTPUT);
-    pinMode(ledPin3, OUTPUT);
-    pinMode(gatePin, OUTPUT);
-    //pinMode(tempPin, INPUT);
-    pinMode(switchPin, INPUT);
+    pinMode(LEDPIN1, OUTPUT);
+    pinMode(LEDPIN2, OUTPUT);
+    pinMode(LEDPIN3, OUTPUT);
+    pinMode(GATEPIN, OUTPUT);
+    pinMode(TEMPPIN, INPUT);
+    pinMode(SWITCHPIN, INPUT_PULLUP);
 
     //	Serial.begin(9600);
     //	mySerial.begin(9600);
@@ -25,4 +39,6 @@ void setup() {
 }
 
 void loop() {
+
+
 }
